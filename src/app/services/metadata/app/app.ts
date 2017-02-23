11ad52dfe777450427a0a0e86783b0e44e74c5e6
Bogdan Carpusor <bcarpusor@cloudbasesolutions.com>
2017-02-23 18:24:05 +0200

    Added metadata mockup service

---
+++ src/app/services/metadata/app/app.ts
@@ -0,0 +1,42 @@
+import 'core-js/library';
+import { Logger, LoggerFactory, RabbitClient } from './common';
+import { Topology } from './config';
+import { Express } from 'express';
+import http = require('http');
+import util = require('util');
+import express = require('express');
+import { ServiceUtils } from './utils';
+
+require('dotenv').config();
+
+const LOGGER: Logger = LoggerFactory.getLogger();
+const app: Express = express();
+
+// Register new instantiated service
+ServiceUtils.registerService();
+const messageHandler = new RabbitClient(
+  Topology.EXCHANGES.servicesExchange,
+  Topology.QUEUES.servicesRequests
+);
+
+
+// Notify a specific service about the message that should be requested
+messageHandler.publishMessage(
+  Topology.MESSAGES.registerPublisher,
+  'NOVA_NOTIFICATIONS',
+  {
+    requestPath: 'api/nova/vms',
+    messageName: Topology.MESSAGES.metadataRequest
+  }
+);
+
+// Reply to incoming subscriber request
+messageHandler.rabbitConnection.handle(Topology.MESSAGES.metadataRequest, message => {
+  LOGGER.info('Received subscriber request');
+  message.body['tags'] = ['tag1', 'tag2'];
+  message.reply(message.body, Topology.MESSAGES.metadataResponse);
+});
+
+app.listen(parseInt(process.env.PORT), () => {
+  LOGGER.info(`Express server listening on port ${process.env.PORT}.`);
+});
---
+++ src/app/services/metadata/app/common/rabbit-client.ts
@@ -0,0 +1,35 @@
+import rabbit = require('rabbot');
+import { Topology } from '../config';
+import { Logger, LoggerFactory, InternalError } from './';
+
+export class RabbitClient {
+  public rabbitConnection = rabbit;
+  private exchangeName: string;
+
+  public static LOGGER: Logger = LoggerFactory.getLogger();
+  constructor(exchangeName: string, queueName: string) {
+    this.exchangeName = exchangeName;
+    Topology.createTopology(this.rabbitConnection)
+      .then(() => {
+        RabbitClient.LOGGER.info('Successfully initialized RabbitMQ connection');
+        this.rabbitConnection.startSubscription(queueName);
+      })
+      .catch((error) => {
+        RabbitClient.LOGGER.error('Error while trying to connect to RabbitMQ');
+        RabbitClient.LOGGER.error(error);
+        throw new InternalError(error);
+      });
+  }
+
+  publishMessage(type: string, routingKey: string, message: any): Promise<any> {
+    return this.rabbitConnection.publish(this.exchangeName, {
+      type: type,
+      routingKey: routingKey,
+      body: message
+    })
+      .then(() => {
+
+        RabbitClient.LOGGER.debug(`Published message - ${JSON.stringify(message)} on ${this.exchangeName}`);
+      });
+  }
+}
