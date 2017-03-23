1c21f522a2c97ee09e7ba8cb253019bf4e10bd73
Bogdan Carpusor <bcarpusor@cloudbasesolutions.com>
2017-03-23 15:27:18 +0200

    Added service registration through message bus

--- app/utils/router-utils.ts
+++ app/utils/router-utils.ts
@@ -1,15 +1,15 @@
-import { InvalidJsonError, NotAuthenticated, ApiError } from '../common';
+import { InvalidJsonError, NotAuthenticated, ApiError, Service } from '../common';
 
 export class RouterUtils {
   /**
    * Middleware to check for a token in the session object
-   * 
+   *
    * @static
-   * @param {any} req 
-   * @param {any} res 
-   * @param {any} next 
-   * @returns {*} 
-   * 
+   * @param {any} req
+   * @param {any} res
+   * @param {any} next
+   * @returns {*}
+   *
    * @memberOf RouterUtils
    */
   static isAuthenticated(req, res, next): any {
@@ -18,6 +18,17 @@ export class RouterUtils {
     } else {
       res.statusCode = 403;
       res.json(new NotAuthenticated().toJSON());
+    }
+  }
+
+  static evaluateNewServiceMessage(messageBody: Service, routes: [string]): Promise<any> {
+    // TODO: Add method to check if a route is already being used
+    if (!messageBody.name || !messageBody.routingPath) {
+      return Promise.reject(`Invalid message body ${JSON.stringify(messageBody)}`)
+  }// else if (routes.indexOf(messageBody.routingPath ) !== -1) {
+  //    return Promise.reject(`An existing service type has already been mounted for ${messageBody.routingPath}`);}
+   else {
+      return Promise.resolve(messageBody);
     }
   }
 }
