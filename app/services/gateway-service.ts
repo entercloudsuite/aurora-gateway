8f2bd5b8042a8f8fc9667a0408a925d097b2dcae
Bogdan Carpusor <bcarpusor@cloudbasesolutions.com>
2017-03-28 17:20:32 +0300

    Added option to add a default api access path

--- app/services/gateway-service.ts
+++ app/services/gateway-service.ts
@@ -73,10 +73,17 @@ export class GatewayService {
     GatewayService.LOGGER.info(`Calling ${serviceHost}, on ${incomingRequest.path}`);
     GatewayService.LOGGER.info(`Request headers - ${JSON.stringify(incomingRequest.headers)})}`);
     GatewayService.LOGGER.info(`Request body - ${JSON.stringify(incomingRequest.body)})}`);
+
+    // A default access path can be set for a service (/api, /apiV2)
+    let callPath = incomingRequest.path;
+    if (apiPath) {
+       callPath = apiPath + incomingRequest.path;
+    }
+
     return GatewayService.sendRequest({
       host: serviceHost,
       port: servicePort,
-      path: incomingRequest.path,
+      path: callPath,
       method: incomingRequest.method,
       headers: incomingRequest.headers,
       body: incomingRequest.body
